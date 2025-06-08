d90b90393e5ee46fdb30a7701519f3b3a8dd0b69
niloy-cyber <nyt252465@gmail.com>
2025-06-08 17:42:07 +0600

    3rd file completed

--- prime_show.py
+++ prime_show.py
@@ -1,1 +1,20 @@
-print("Here you can see the prime numbers as you want")
+from math import *
+
+print("Here you can see the prime numbers as you want.")
+a = int(input("Enter the starting number : "))
+b = int(input("Enter the ending number : "))
+
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,int(sqrt(n))+1):
+        if n%i == 0:
+            return False
+    return True
+x  = 0
+for j in range(a,b+1):
+    if prime(j):
+        print(j,end=" ")
+        x += 1
+print()
+print("Total prime numbers are", x)
